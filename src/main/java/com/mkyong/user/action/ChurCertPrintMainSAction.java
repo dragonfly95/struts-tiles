050eaa7216b0a87b3a0c0353bedc307695514621
dragonfly95 <nosignal95@gmail.com>
2017-09-26 00:19:43 +0900

    Update ChurCertPrintMainSAction.java

--- src/main/java/com/mkyong/user/action/ChurCertPrintMainSAction.java
+++ src/main/java/com/mkyong/user/action/ChurCertPrintMainSAction.java
@@ -18,62 +18,62 @@ import org.apache.struts.action.ActionMapping;
 
 public class ChurCertPrintMainSAction extends SignonDispatchAction {
 
-	private DutyDAO dutyDAO;
-	private DutyManager dutyManager;
+  private DutyDAO dutyDAO;
+  private DutyManager dutyManager;
 
   public ChurCertPrintMainSAction() {
-  	dutyDAO = new DutyDAO();
-		dutyManager = new DutyManager();
-	}
+    dutyDAO = new DutyDAO();
+    dutyManager = new DutyManager();
+  }
 
-	public ActionForward first(
-					ActionMapping mapping,	ActionForm form,
-					HttpServletRequest request,	HttpServletResponse response) throws Exception {
-		return unspecified(mapping,form,request,response);
-	}
-
-
-	public ActionForward view(
-					ActionMapping mapping,	ActionForm form,
-	  HttpServletRequest request,	HttpServletResponse response) throws Exception {
-		return mapping.findForward("view");
-	}
+  public ActionForward first(
+          ActionMapping mapping,  ActionForm form,
+          HttpServletRequest request,  HttpServletResponse response) throws Exception {
+    return unspecified(mapping,form,request,response);
+  }
 
 
-	protected ActionForward unspecified(
-					ActionMapping mapping,	ActionForm form,
-					HttpServletRequest request,	HttpServletResponse response) throws Exception
-	{
-		String cpage = request.getParameter("cpage");
-
-		if("".equals(cpage) || cpage == null) {
-			cpage = "1";
-			map.put("cpage", new Integer(cpage));
-		}
-
-		if(!"".equals(map.get("key"))) {
-			if("sname".equals(map.get("key"))) map.put("sname", map.get("keyword"));
-			if("j_chSearch".equals(map.get("key"))) map.put("j_chSearch", map.get("keyword"));
-		}
+  public ActionForward view(
+          ActionMapping mapping,  ActionForm form,
+    HttpServletRequest request,  HttpServletResponse response) throws Exception {
+    return mapping.findForward("view");
+  }
 
 
-		map.put("kk_code", kkCode);
+  protected ActionForward unspecified(
+          ActionMapping mapping,  ActionForm form,
+          HttpServletRequest request,  HttpServletResponse response) throws Exception
+  {
+    String cpage = request.getParameter("cpage");
 
-		ListHandler listHandler = new ListHandler();
-		map.put("pageSize", new Integer(listHandler.getPageSize()));
+    if("".equals(cpage) || cpage == null) {
+      cpage = "1";
+      map.put("cpage", new Integer(cpage));
+    }
+
+    if(!"".equals(map.get("key"))) {
+      if("sname".equals(map.get("key"))) map.put("sname", map.get("keyword"));
+      if("j_chSearch".equals(map.get("key"))) map.put("j_chSearch", map.get("keyword"));
+    }
 
 
-		int totalCount = dutyManager.getTotalCount(map);
-		request.setAttribute("CertList", dutyManager.getDutyList(map));
-		request.setAttribute("CertTotal",new Integer(totalCount));
+    map.put("kk_code", kkCode);
 
-		listHandler.setCpage(new Integer(cpage).intValue());
-		listHandler.setTotal(totalCount);
-		listHandler.setPageSize(listHandler.getPageSize());
-		listHandler.setLink("./churCertPrintMainS.do?act=first");
+    ListHandler listHandler = new ListHandler();
+    map.put("pageSize", new Integer(listHandler.getPageSize()));
 
-		request.setAttribute("numbering", listHandler.getNumbering());
-		return mapping.findForward("executeQueryForList");
-	}
+
+    int totalCount = dutyManager.getTotalCount(map);
+    request.setAttribute("CertList", dutyManager.getDutyList(map));
+    request.setAttribute("CertTotal",new Integer(totalCount));
+
+    listHandler.setCpage(new Integer(cpage).intValue());
+    listHandler.setTotal(totalCount);
+    listHandler.setPageSize(listHandler.getPageSize());
+    listHandler.setLink("./churCertPrintMainS.do?act=first");
+
+    request.setAttribute("numbering", listHandler.getNumbering());
+    return mapping.findForward("executeQueryForList");
+  }
 
 }
